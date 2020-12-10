191bff91fa8ec143a350173a120d38e0c4acbea9
“caoting43” <Soft.cao@outlook.com>
2020-12-10 17:33:39 +0800

    防护结束

--- ihome/web_html.py
+++ ihome/web_html.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, current_app
+from flask import Blueprint, current_app, make_response
+from flask_wtf import csrf
 
 # 提供静态文件的蓝图
 html = Blueprint("web_html", __name__)
@@ -20,5 +21,13 @@ def get_html(html_file_name):
     if html_file_name != "favicon.ico":
         html_file_name = "html/" + html_file_name
 
+    # 创建csrf_token值
+    csrf_token = csrf.generate_csrf()
+
     # flask提供的返回静态文件的方法
-    return current_app.send_static_file(html_file_name)
+    resp = make_response(current_app.send_static_file(html_file_name))
+
+    # 设置cookie值
+    resp.set_cookie("csrf_token", csrf_token)
+
+    return resp
