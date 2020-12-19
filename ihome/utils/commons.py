91ac2d555154fdf93dc61d4eb480aab0109252e6
“caoting43” <Soft.cao@outlook.com>
2020-12-19 10:35:05 +0800

    七牛云上传图片的使用

--- ihome/utils/commons.py
+++ ihome/utils/commons.py
@@ -1,5 +1,7 @@
 from werkzeug.routing import BaseConverter
-
+from flask import session, jsonify, g
+from ihome.utils.response_code import RET
+import functools
 
 # 定义怎则表达式
 class ReConverter(BaseConverter):
@@ -9,3 +11,30 @@ class ReConverter(BaseConverter):
         super(ReConverter, self).__init__(url_map)
         # 保存正则表达式
         self.regex = reqex
+
+
+# 定义的验证登录状态的装饰器
+def login_required(view_func):
+    @functools.wraps(view_func)
+    def wrapper(*args, **kwargs):
+        # 判断用户的登录状态
+        user_id = session.get("user_id")
+
+        # 如果用户是登录的，执行视图函数
+        if user_id is not None:
+            # 将user_id保存到g对象中，在视图函数中可以通过g对象获取保存数据
+            g.user_id = user_id
+            return view_func(*args, **kwargs)
+        else:
+            # 如果未登录，返回未登录的信息
+            return jsonify(errno=RET.SESSIONERR, errmsg="用户未登录")
+
+    return wrapper
+
+
+@login_required
+def set_user_avatar():
+    user_id = g.user_id
+    return
+
+# set_user_avatar() -> wrapper()
